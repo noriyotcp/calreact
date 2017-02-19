c0f5babd8d7895b6fc5eb701051e65076b2a2e61
Noriyo Akita <noriyo.akita@gmail.com>
2017-02-19 16:40:23 +0000

    Enable to input a new appointment and to render a page without refreshing

--- app/assets/javascripts/components/appointment_form.jsx
+++ app/assets/javascripts/components/appointment_form.jsx
@@ -9,17 +9,23 @@ var AppointmentForm = React.createClass({
     obj[name] = e.target.value;
     this.props.onUserInput(obj);
   },
+
+  handleSubmit: function(e) {
+    e.preventDefault();
+    this.props.onFormSubmit();
+  },
+
   render: function() {
     return (
       <div>
         <h2>Make a new appointment</h2>
 
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <input name="title" placeholder='Appointment Title'
-            value={this.props.input_title}
+            value={this.props.title}
             onChange={this.handleChange} />
           <input name="appt_time" placeholder='Date and Time'
-            value={this.props.input_appt_time}
+            value={this.props.appt_time}
             onChange={this.handleChange} />
           <input type='submit' value='Make appointment' />
         </form>
--- app/assets/javascripts/components/appointments.jsx
+++ app/assets/javascripts/components/appointments.jsx
@@ -1,5 +1,6 @@
 // Avoid warning on cloud9
 /* global React */
+/* global $ */
 
 var Appointments = React.createClass({
   getInitialState: function() {
@@ -14,12 +15,27 @@ var Appointments = React.createClass({
     this.setState(obj);
   },
 
+  handleFormSubmit: function() {
+    var appointment = { title: this.state.title, appt_time: this.state.appt_time };
+    $.post('/appointments',
+            { appointment: appointment })
+            .done(function(data) {
+              this.addNewAppointment(data);
+            }.bind(this));
+  },
+
+  addNewAppointment: function(appointment) {
+    var appointments = React.addons.update(this.state.appointments, { $push: [appointment] });
+    this.setState({ appointments: appointments });
+  },
+
   render: function() {
     return (
       <div>
-        <AppointmentForm input_title={this.state.title}
-          input_appt_time={this.state.appt_time}
-          onUserInput={this.handleUserInput} />
+        <AppointmentForm title={this.state.title}
+          appt_time={this.state.appt_time}
+          onUserInput={this.handleUserInput}
+          onFormSubmit={this.handleFormSubmit} />
         <AppointmentsList appointments={this.state.appointments} />
       </div>
     );

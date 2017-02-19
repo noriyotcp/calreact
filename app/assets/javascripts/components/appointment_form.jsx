1103b8a8df095984825036c02c9c592ed8cdc9d4
Noriyo Akita <noriyo.akita@gmail.com>
2017-02-19 15:52:57 +0000

    Enable to set state of Appointments component with user input

--- app/assets/javascripts/components/appointment_form.jsx
+++ app/assets/javascripts/components/appointment_form.jsx
@@ -1,15 +1,26 @@
 // Avoid warning on cloud9
 /* global React */
+/* global obj */
 
 var AppointmentForm = React.createClass({
+  handleChange: function(e) {
+    var name = e.target.name;
+    obj = {};
+    obj[name] = e.target.value;
+    this.props.onUserInput(obj);
+  },
   render: function() {
     return (
       <div>
         <h2>Make a new appointment</h2>
 
         <form>
-          <input name="title" placeholder='Appointment Title' />
-          <input name="appt_time" placeholder='Date and Time' />
+          <input name="title" placeholder='Appointment Title'
+            value={this.props.input_title}
+            onChange={this.handleChange} />
+          <input name="appt_time" placeholder='Date and Time'
+            value={this.props.input_appt_time}
+            onChange={this.handleChange} />
           <input type='submit' value='Make appointment' />
         </form>
       </div>
